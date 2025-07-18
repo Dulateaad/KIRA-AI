c1deed3377ab3dab310b14818bcf1292916cb2f0
Dulateaad <asdfsdassdsdd@gmail.com>
2025-07-18 17:14:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ bot = TeleBot("7828546240:AAFO_-dgTpbDQiUXRsx3CTj1FlZdRsUD11Q")
 @bot.message_handler(commands=["start"])
 def start(message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    btn = KeyboardButton(text="Открыть Mini App", web_app=WebAppInfo(url="https://studio.firebase.google.com/studio-13031746"))
+    btn = KeyboardButton(text="Открыть Mini App", web_app=WebAppInfo(url="https://studio--styleai-pzp45.us-central1.hosted.app/"))
     markup.add(btn)
     bot.send_message(message.chat.id, "Нажми кнопку, чтобы открыть приложение:", reply_markup=markup)
 
